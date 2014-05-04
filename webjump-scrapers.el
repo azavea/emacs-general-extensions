98e605cfb42795b56a232eb2a6a3bf826a94a04e
steve <steventlamb@gmail.com>
2014-05-04 16:40:24 -0400

    Add builtin support for jquery doc scraping

--- webjump-scrapers.el
+++ webjump-scrapers.el
@@ -7,22 +7,30 @@
 
 (defconst wjs/builtins
   '(("underscore" .
-     (wjs/query!! "underscorejs.org" :capture wjs/id-capture :drop ls/version-number))
+     (wjs/query!! "underscorejs.org" wjs/hash-url-builder :capture wjs/id-capture :drop ls/version-number))
+
+    ("jquery" .
+     (wjs/query!! "api.jquery.com"
+                  (lambda (base-url item) (concat base-url "/" item))
+                  :capture "^.*href=\"http://api.jquery.com/\\([^/]+\\)/\""))
+
     ("baconjs" .
      (wjs/query!! "https://github.com/baconjs/bacon.js/tree/master"
+                  wjs/hash-url-builder
                   :capture wjs/github-href-capture))))
 
 (defconst wjs/id-capture "^.*id=\"\\([^\"]+\\)\".*$")
 (defconst  wjs/github-href-capture "^.*href=\"#\\([^\"]+\\)\".*$")
+(defconst wjs/hash-url-builder (lambda (base-url dom-id) (concat base-url "/#" dom-id)))
 
-(defun wjs/query! (base-url &rest transformers)
+(defun wjs/query! (base-url url-builder &rest transformers)
   (let* ((page (ls/read-url! base-url))
          (completions (apply 'ls/scrape-body page transformers))
-        (id (completing-read "id: " completions)))
-    (concat base-url "/#" id)))
+         (completion (completing-read "id: " completions)))
+    (funcall url-builder base-url completion)))
 
-(defun wjs/query!! (base-url &rest actions)
-  (apply 'wjs/query! base-url (apply 'ls/parse-actions actions)))
+(defun wjs/query!! (base-url url-builder &rest actions)
+  (apply 'wjs/query! base-url url-builder (apply 'ls/parse-actions actions)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; builtin scrapes
