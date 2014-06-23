94aff998bbc768886a6e8a84cf9c7c83f81f2f19
Steve Lamb <slamb@azavea.com>
2014-06-23 17:41:46 -0400

    Add support for keeping intermediate values
    fixes #8 on github

--- line-scraper-test.el
+++ line-scraper-test.el
@@ -1,22 +1,72 @@
+;;; line-scraper -*- lexical-binding: t -*-
+
 (require 'line-scraper)
 (require 'test-utils)
 (require 'ert)
 
+(defun ls/test/helpers/drop (pattern value)
+  (funcall (funcall (plist-get ls/recipe-aliases :drop) pattern) value))
 
-(ert-deftest ls/regexp-drop ()
-  (should (equal (funcall (ls/regexp-drop "^foo") nil) nil))
-  (should (equal (funcall (ls/regexp-drop "nil") nil) nil))
-  (should (equal (funcall (ls/regexp-drop "^foo") "foo") nil))
-  (should (equal (funcall (ls/regexp-drop "^foo") "foso") "foso")))
+(ert-deftest ls/test/drop ()
+  (should (equal (ls/test/helpers/drop "^foo" nil) nil))
+  (should (equal (ls/test/helpers/drop "^foo" "foo") nil))
+  (should (equal (ls/test/helpers/drop "^foo" "foto") "foto")))
 
-(ert-deftest ls/parse-actions ()
-  (should (eval-equal '("foo" "foso" "1.2.3" nil)
-                      (ls/regexp-drop "^foo")
-                      (car (ls/parse-actions :drop "^foo")))))
+(ert-deftest ls/test/parse-compound-keyword ()
+  (should (equal (ls/parse-compound-keyword :foo) '(:foo)))
+  (should (equal (ls/parse-compound-keyword :foo:bar) '(:foo :bar))))
+
+(ert-deftest ls/test/parse-action-pair ()
+  (let* ((results (ls/parse-action-pair '(:drop "^foo")))
+         (transformer (plist-get results :transformer))
+         (intermediate-value-keys (plist-get results :intermediate-value-keys)))
+    (should (equal nil intermediate-value-keys))
+    (should (equal "foto" (funcall transformer "foto"))))
+  (let* ((results (ls/parse-action-pair '(:drop:capture-on "^foo")))
+         (transformer (plist-get results :transformer))
+         (intermediate-value-keys (plist-get results :intermediate-value-keys)))
+    (should (equal '(:capture-on) intermediate-value-keys))
+    (should (equal "foto" (funcall transformer "foto")))))
 
 
-(ert-deftest ls/scrape-body ()
-  (let ((f (lambda (arg) (concat arg "BAZ!"))))
-  (should (equal (ls/scrape-body "foo\nbar") '("foo" "bar")))
-  (should (equal (ls/scrape-body "foo\nbar" f) '("fooBAZ!" "barBAZ!")))
-))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ls/parse-recipe
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ert-deftest ls/test/parse-recipe/basic-recipe ()
+  (let ((results (ls/parse-recipe '(:drop "^foo"))))
+    (should (equal nil
+                   (plist-get results :intermediate-value-indices)))
+    (should (equal "foto"
+                   (funcall (car (plist-get results :transformers)) "foto"))))
+)
+
+(ert-deftest ls/test/parse-recipe/single-intermediate-value ()
+  (let ((results (ls/parse-recipe '(:drop "^foo" :keep:capture-on "bar$"))))
+    (should (equal '(:capture-on 1)
+                   (plist-get results :intermediate-value-indices))))
+)
+(ert-deftest ls/test/parse-recipe/multiple-intermediate-value-on-transformer ()
+  (let ((results (ls/parse-recipe '(:drop "^foo" :keep:complete-on:build-url-on "bar$"))))
+    (should (equal '(:complete-on 1 :build-url-on 1)
+                   (plist-get results :intermediate-value-indices))))
+)
+
+(ert-deftest ls/test/parse-recipe/multiple-intermediate-value-on-transformer ()
+  (let ((results (ls/parse-recipe '(:drop "^foo" :keep:complete-on:build-url-on "bar$"))))
+    (should (equal '(:complete-on 1 :build-url-on 1)
+                   (plist-get results :intermediate-value-indices))))
+)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ls/scrape
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ert-deftest ls/test/scrape ()
+  (should (equal '(:final-values (nil "bar" "baz") :intermediate-values nil)
+                 (ls/scrape "foo\nbar\nbaz\n" '(:drop "foo"))))
+  (should (equal '(:final-values (nil "bar" nil) :intermediate-values (:capture-on (nil "bar" "baz")))
+                 (ls/scrape "foo\nbar\nbaz\n" '(:drop:capture-on "foo" :keep "bar"))))
+  (should (equal '(:final-values (nil "bar" nil) :intermediate-values (:capture-on (nil "bar" "baz")))
+                 (ls/scrape "foo\nbar\nbaz\n" '(:fn (lambda (arg) arg) :drop:capture-on "foo" :keep "bar")))))
--- line-scraper.el
+++ line-scraper.el
@@ -1,66 +1,120 @@
-;;; line-scraper  -*- lexical-binding: t -*-
+;;; line-scraper -*- lexical-binding: t -*-
 
-;; provide tools for taking a body of text and transforming lines
-;; to produce a structured list of output strings, typically for
+;; provide tools for taking a body of text and transforming lines to
+;; produce a structured list of output strings, typically for
 ;; completion libraries.
 
-;; provides a bunch of utilities for promoting regular expressions
-;; to transformer functions, and a DSL for expressing transformations
+;; provides a bunch of utilities for promoting regular expressions to
+;; transformer functions, and a DSL for expressing transformations
 ;; succinctly using keywords.
 
+;; :recipe should be a plist, with keyword args :capture, :keep,
+;; :drop, :fn.  if the arg is a :fn, it will be applied. otherwise, it
+;; should be a regexp to lift into a fn.  additionally, any of these
+;; keywords can be appended like so, to complete-on, or to build
+;; arg-on: :capture:complete-on <pattern>
+
+(require 'history)
 (require 'dash)
+(require 'dash-functional)
 (require 'f)
 (require 's)
 
-(defconst ls/version-number "\\([0-9]+\.\\)+[0-9]+")
-
-(defconst ls/action-aliases
-  '(:keep ls/regexp-keep
-    :drop ls/regexp-drop
-    :capture ls/regexp-capture))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;
-;; regexp utils
-;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun ls/regexp-drop (pattern)
-  (lambda (value)
-    (if (null value) nil
-      (if (nth 0 (s-match pattern value)) nil value))))
-
-(defun ls/regexp-keep (pattern)
-  (lambda (value)
-    (if (null value) nil
-    (nth 0 (s-match pattern value)))))
-
-(defun ls/regexp-capture (pattern)
-  (lambda (value)
-    (if (null value) nil
-      (nth 1 (s-match pattern value)))))
-
-;; unused, but maybe useful
-(defun ls/capturify-regexp (pattern)
-  "Take a match regexp and make it a capture regexp"
-  (concat "\\(" pattern "\\)"))
+(defconst ls/recipe-aliases
+  '(:fn (lambda (fn) fn)
+    :keep (lambda (pattern)
+            `(closure
+              ((pattern . ,pattern) t) (value)
+              (if (null value) nil
+                (nth 0 (s-match pattern value)))))
+    :drop (lambda (pattern)
+            `(closure
+              ((pattern . ,pattern) t) (value)
+              (if (null value) nil
+                (if (nth 0 (s-match pattern value)) nil value))))
+    :capture (lambda (pattern)
+               `(closure
+                 ((pattern . ,pattern) t) (value)
+                 (if (null value) nil
+                   (nth 1 (s-match pattern value)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; general utils
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun ls/parse-actions (&rest actions)
-  (let ((pairs (-partition 2 actions))
-        (fn (lambda (pair)
-              (funcall (plist-get ls/action-aliases (car pair)) (nth 1 pair)))))
-    (-map fn pairs)))
+(defun ls/parse-compound-keyword (keyword)
+  "turn a keyword into a list of keywords, so that :foo becomes (:foo)
+and :foo:bar becomes (:foo :bar)"
+  (--map (intern (concat ":" it))
+         (s-split ":" (symbol-name keyword) :omit-empty-strings)))
 
-(defun ls/scrape-body (page &rest transformers)
-  "take a body of text as a string, apply each transformer in
-succession to each line.  Filter out lines that transformed to nil,
-and return a list of the results."
-  (let ((tokens! (s-lines page)))
-    (dolist (transformer transformers)
-      (setq tokens! (-map transformer tokens!)))
-    tokens!))
+(defun ls/parse-action-pair (pair)
+  (let* ((keywords (ls/parse-compound-keyword (car pair)))
+         (keyword (car keywords))
+         (intermediate-value-keys (cdr keywords))
+         (pattern (nth 1 pair))
+         (pattern-lifter (plist-get ls/recipe-aliases keyword))
+         (transformer (funcall pattern-lifter pattern)))
+    (list :transformer transformer
+          :intermediate-value-keys intermediate-value-keys)))
+
+(defun ls/parse-recipe (recipe)
+  "takes a recipe, returns a plist of transformers, and intermediate value indices.
+
+ex:
+\\(ls/parse-recipe '(:drop foo :drop:capture-on bar :fn 'baz)) =>
+
+\\(:transformers (drop-foo-lambda 'drop-bar-lambda 'baz)
+ :intermediate-value-indices (:capture-on 1))
+"
+  ;; split the plist pairs of the recipe
+  (let* ((pairs (-partition 2 recipe))
+         (pair-results (-map 'ls/parse-action-pair pairs))
+
+         ;; intialize components of return type
+         (transformers (list))
+         (intermediate-value-indices (list))
+
+         ;; create iteration index
+         (pair-index 0))
+
+    ;; iterate over each parsed pair, add the transformers and value indices, if any.
+    (dolist (pair-result pair-results)
+      (let ((transformer (plist-get pair-result :transformer))
+            (intermediate-value-keys (plist-get pair-result :intermediate-value-keys)))
+        (setq transformers (cons transformer transformers))
+        (when intermediate-value-keys
+          (dolist (intermediate-value-key intermediate-value-keys)
+            (setq intermediate-value-indices
+                  (plist-put intermediate-value-indices intermediate-value-key pair-index)))))
+      (setq pair-index (+ 1 pair-index)))
+
+    ;; return transformers and value indices as a plist
+    (list :transformers (nreverse transformers)
+          :intermediate-value-indices intermediate-value-indices)))
+
+(defun ls/scrape (body recipe)
+  "Take a page and a recipe. Return a plist of the finished,
+  transformed value according to the recipe, as well as any
+  intermediate lists that were specified by compound keyword arguments
+  in the recipe."
+  (let* ((parsed-recipe (ls/parse-recipe recipe))
+         (transformers (plist-get parsed-recipe :transformers))
+         (intermediate-value-indices
+          (plist-get parsed-recipe :intermediate-value-indices))
+         (line-histories (-map 'hist/init (-filter 's-present? (s-lines body))))
+         (transformed-histories (-map (apply 'hist/compose (nreverse transformers)) line-histories))
+         (final-values (-map 'hist/get transformed-histories))
+         (intermediate-values (list)))
+    (--each (-partition 2 intermediate-value-indices)
+      (let* ((key (car it))
+             (value (nth 1 it))
+             (intermediate-value-list (list)))
+        (dolist (transformed-history transformed-histories)
+          (setq intermediate-value-list (cons (hist/get-when transformed-history (1+ value)) intermediate-value-list)))
+        (setq intermediate-values (plist-put intermediate-values key (nreverse intermediate-value-list)))
+        ))
+    (list :final-values final-values :intermediate-values intermediate-values)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -69,12 +123,12 @@ and return a list of the results."
 
 (defun ls/read-url! (url)
   "a thin wrapper around curl"
-  (let ((curl-command (s-format "curl $0 2>/dev/null" 'elt (list url))))
-  (shell-command-to-string curl-command)))
+  (let* ((curl-command (s-format "curl $0 2>/dev/null" 'elt (list url)))
+         (body (shell-command-to-string curl-command)))
+    (copy-sequence body)))
 
 (defalias 'ls/read-file! 'f-read-text)
 
 (defalias 'ls/read-command! 'shell-command-to-string)
 
 (provide 'line-scraper)
-
